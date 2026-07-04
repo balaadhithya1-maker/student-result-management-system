ea0eeb8628dd6625d8ebd479777702c07ed75a08
balaadhithya1-maker <balaadhithya1@gmail.com>
2026-07-04 09:26:19 +0000

    Day 4: Added Docker support for backend

--- backend/app.py
+++ backend/app.py
@@ -55,5 +55,4 @@ def result(reg):
     return {"message": "Student Not Found"}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-
+    app.run(host="0.0.0.0", port=5001, debug=False)
